701d5b9e84b2af6c65ca96fbce83bcc1d1e78268
osama seid <osaseid1@gmail.com>
2024-10-16 12:34:29 +0300

    ecommercce -api-products

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kmhf_3grb2ldm(bsx_&=l!dl-#%p$=u)*&m4dvk0d^!19(m%1z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['osa.pythonanywhere.com']
 
 
 # Application definition
