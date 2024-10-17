2e8a2ce8faa028361ac1bd0da1dec7d5300e458a
osama seid <osaseid1@gmail.com>
2024-10-17 11:39:48 +0300

    everything updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -88,22 +88,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 
-        'NAME': 'becapstone',           #
-        'USER': 'root',                
-        'PASSWORD': '0924475152@os',          
-        'HOST': 'localhost',                    
-        'PORT': '3306',                         
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
